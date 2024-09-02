1631810ba6c47042dc0d99e44bd7cd5943eba881
Satyam <nakulbegana191170@gmail.com>
2024-09-02 20:04:27 +0000

    Generate lifts and their initial position

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 function generateLiftAndFloors() {
     const liftCount = document.getElementById("lift-count").valueAsNumber || 2;
     const floorCount = document.getElementById("floor-count").valueAsNumber || 4;
-    const liftsAndFloorWrapper = document.getElementById("lifts");
+    const liftsAndFloorWrapper = document.getElementById("lifts-area");
     console.log(liftCount, floorCount);
 
     liftsAndFloorWrapper.innerHTML = "";
     liftsAndFloorWrapper.appendChild(createFloor(floorCount));
+    liftsAndFloorWrapper.appendChild(createLifts(liftCount));
 }
 
 function createFloor(floorCount) {
@@ -23,6 +24,23 @@ function createFloor(floorCount) {
         `;
 
         frag.innerHTML += floorHTML;
+    }
+
+    return frag;
+}
+
+function createLifts(liftCount) {
+    const frag = document.createElement("div");
+
+    for (let count = liftCount - 1; count >= 0; count--) {
+        const liftHTML = `
+            <div class="lift" style="left: ${(liftCount - count) * 120}px">
+                <div class="lift-door"></div>
+                <div class="lift-door"></div>
+            </div>
+        `;
+
+        frag.innerHTML += liftHTML;
     }
 
     console.log(frag)
